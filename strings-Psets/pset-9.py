4d9c98302ba8871e318437b3bda038698fcb3b97
Sujith Acharya <sujithacharya9@gmail.com>
2023-01-06 22:21:32 +0530

    Add File pset-9.py

---
+++ strings-Psets/pset-9.py
@@ -0,0 +1,6 @@
+str_in =input("Enter the String containing letter 'a'\n")
+
+#Replace every occurrence of the character "a" with "b"
+newstr_in = str_in.replace('a','b')
+print("The new string after replacement is", newstr_in)
+
